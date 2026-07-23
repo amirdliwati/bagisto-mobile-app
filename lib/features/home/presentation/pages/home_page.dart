51c22dc2482ca94752357105728657714dfdd12a
Dr. Mohamad Amir Dliwati <mohamad.amir.dliwati@gmail.com>
2026-07-23 18:41:44 +0300

    Replace Bagisto logo with Frontier logo in home page and add new SVG asset

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -317,13 +317,13 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                     Row(
                       children: [
                         SvgPicture.asset(
-                          'assets/images/bagisto_logo.svg',
+                          'assets/images/frontier_logo.svg',
                           width: 20,
                           height: 20,
                         ),
                         const SizedBox(width: 4),
                         Text(
-                          'bagisto',
+                          'frontier',
                           style: TextStyle(
                             fontFamily: 'Montserrat',
                             fontWeight: FontWeight.w700,
