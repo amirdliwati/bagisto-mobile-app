fdb5934d1b54adc91288f3691329d4a2570c56bd
Dr. Mohamad Amir Dliwati <mohamad.amir.dliwati@gmail.com>
2026-08-05 02:48:59 +0300

    fix: update storefront key to an empty string in api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -4,7 +4,7 @@ const String bagistoEndpoint =
 
 /// Storefront key for Bagisto API
 const String storefrontKey =
-    'tFVCf+jSp4RJgY985gJQD+XYd8RxoOpDf36ds05YPJI=';
+    '';
 
 /// Default channel code used by request headers.
 const String channelCode = 'default';
